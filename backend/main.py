779126b0e37ce5535a25cbb4cfe436076169dcb7
Maksim Kastsiankou <off.mayson.off@gmail.com>
2025-03-05 17:22:14 +0100

    databaseLIVE

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
-from app.database.database import SessionLocal  # важно, именно так, не backend.app.database.database
+from app.database.database import SessionLocal
+from sqlalchemy import text
 
 app = FastAPI()
+
 
 @app.get("/test-db")
 def test_db():
     try:
         db = SessionLocal()
-        db.execute("SELECT 1")
+        db.execute(text("SELECT 1"))
         db.close()
         return {"status": "Подключение к базе данных успешно"}
     except Exception as e:
